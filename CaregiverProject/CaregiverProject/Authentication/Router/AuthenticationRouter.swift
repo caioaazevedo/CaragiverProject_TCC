89838657dbfcbc03bf0a42c07a222998d435827d
Caio Azevedo <caioba19@gmail.com>
2020-11-03 11:45:09 -0300

    Create Authentication Router

---
+++ CaregiverProject/CaregiverProject/Authentication/Router/AuthenticationRouter.swift
@@ -0,0 +1,29 @@
+//
+//  AuthenticationRouter.swift
+//  CaregiverProject
+//
+//  Created by Caio Azevedo on 03/11/20.
+//
+
+import UIKit
+
+class AuthenticationRouter: PresenterToRouterAuthenticationProtocol {
+    static func createModule() -> AuthenticationViewController {
+        let view = AuthenticationViewController()
+        let presenter: ViewToPresenterAuthenticationProtocol & InteractorToPresenterAuthenticationProtocol = AuthenticationPresenter()
+        let interactor: PresenterToInteractorAuthenticationProtocol = AuthenticationInteractor()
+        let router: PresenterToRouterAuthenticationProtocol = AuthenticationRouter()
+        
+        view.presenter = presenter
+        presenter.view = view
+        presenter.interactor = interactor
+        presenter.router = router
+        interactor.presenter = presenter
+        
+        return view
+    }
+    
+    func pushToMovieScreen() {
+        // Start Family
+    }
+}
